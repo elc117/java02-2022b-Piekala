ac6474c807d7cc097099ee8a7c217161ffdf35c7
Leonardo Piekala <37380454+Piekala@users.noreply.github.com>
2022-09-27 12:00:51 -0300

    Criação do arquivo da atividade em dupla

---
+++ Animes.java
@@ -0,0 +1,38 @@
+class Anime{
+
+    private string nome;
+    private boolean diasAtuais;
+    private int ano;
+
+    Anime(){
+        nome = "";
+        diasAtuais = false;
+        ano = 0;
+    }
+    
+    public string getNome(){
+        return nome;
+    }
+
+    public boolean getDiasAtuais(){
+        return diasAtuais;
+    }
+    
+    public int getAno(){
+        return ano;
+    }
+
+    public void setNome( string nome ){
+        this.nome = nome;
+    }
+
+    public void setDiasAtuais( boolean _diasAtuais ){
+        diasAtuais = _diasAtuais;
+    }
+    
+    public void setAno( int ano ){
+        this.ano = ano;
+    }
+
+    public 
+}
