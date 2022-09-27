e36828332112f03fa927b6b9b696033d5e00965e
Leonardo Piekala <37380454+Piekala@users.noreply.github.com>
2022-09-27 13:41:15 -0300

    Update TestAnime.java

--- TestAnime.java
+++ TestAnime.java
@@ -2,7 +2,15 @@ class testAnime{
 
   int main(){
     Anime anime1 = new Anime();
-    Anime anime2 = new Anime("Boku no hero", true, 2016);
+    Anime anime2 = new Anime("Boku no hero", true, 0);
+    
+    anime1.setNome("Neon Genesis Evangelion");
+    anime1.setDiasAtuais(false);
+    anime1.setAno(1995);
+    
+    anime2.estaAtivoHoje();
+    anime2.setAno(2016);
+    system.out.println(anime2.anosDesdeCriacao());
   }
 
 }
